ee050e9f7c914d6e30a845d7e24872eda633e873
ryun89 <115915990+ryun89@users.noreply.github.com>
2024-07-06 12:30:48 +0900

    Initial Commit

---
+++ LeftoverChefApp/LeftoverChefAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftoverChefAppApp.swift
+//  LeftoverChefApp
+//
+//  Created by 八久響 on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct LeftoverChefAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
