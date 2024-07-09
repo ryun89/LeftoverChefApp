b870743acd0c310cee58e0cd9d6ff748135ad40c
ryun89 <115915990+ryun89@users.noreply.github.com>
2024-07-09 15:03:40 +0900

    レシピ一覧の表示をセーフエリア内に表示するように修正

--- LeftoverChefApp/Views/SearchResultView.swift
+++ LeftoverChefApp/Views/SearchResultView.swift
@@ -13,59 +13,65 @@ struct SearchResultView: View {
     private let DISPLAY_LIMIT = 5
     
     var body: some View {
-        VStack {
-            // 検索結果一覧をリストで表示する
-            List(recipeViewModel.recipes, id: \.label) { recipe in
-                Button {
-                    recipeViewModel.recipeLink = URL(string: recipe.url)
-                    isShowSafari.toggle()
-                    
-                } label: {
-                    HStack {
-                        VStack(alignment: .leading){
-                            Text(recipe.label)
-                                .font(.headline)
-                            Text("調理時間:\(recipe.totalTime)分")
-                                .font(.body)
-                            Text("使用する食材:")
-                                .font(.subheadline)
-                            ForEach(recipe.ingredientLines.prefix(DISPLAY_LIMIT), id: \.self) { ingredient in
-                                Text(ingredient)
-                                    .font(.subheadline)
-                            }
-                            if recipe.ingredientLines.count > DISPLAY_LIMIT {
-                                Text("...")
+        GeometryReader { geometry in
+            VStack {
+                // セーフエリアの上部のインセットを取得
+                let topPadding = geometry.safeAreaInsets.top
+                
+                // 検索結果一覧をリストで表示する
+                List(recipeViewModel.recipes, id: \.label) { recipe in
+                    Button {
+                        recipeViewModel.recipeLink = URL(string: recipe.url)
+                        isShowSafari.toggle()
+                        
+                    } label: {
+                        HStack {
+                            VStack(alignment: .leading){
+                                Text(recipe.label)
+                                    .font(.headline)
+                                Text("調理時間:\(recipe.totalTime)分")
                                     .font(.body)
+                                Text("使用する食材:")
+                                    .font(.subheadline)
+                                ForEach(recipe.ingredientLines.prefix(DISPLAY_LIMIT), id: \.self) { ingredient in
+                                    Text(ingredient)
+                                        .font(.subheadline)
+                                }
+                                if recipe.ingredientLines.count > DISPLAY_LIMIT {
+                                    Text("...")
+                                        .font(.body)
+                                }
                             }
-                        }
-                        Spacer()
-                        if let imageUrl = URL(string: recipe.image) {
-                            AsyncImage(url: imageUrl) { img in
-                                img
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 150, height: 150)
-                            } placeholder: {
-                                ProgressView()
+                            Spacer()
+                            if let imageUrl = URL(string: recipe.image) {
+                                AsyncImage(url: imageUrl) { img in
+                                    img
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 150, height: 150)
+                                } placeholder: {
+                                    ProgressView()
+                                }
                             }
                         }
                     }
                 }
+                .padding(.top, topPadding) // セーフエリアの上部のパディングを追加
+                .background(Color.orange) // Listの背景色を設定
+                .listStyle(PlainListStyle()) // Listのスタイルを設定
+                
+                .navigationBarTitle("検索結果", displayMode: .inline)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarBackground(Color.orange, for: .navigationBar)
+                
+                .sheet(isPresented: $isShowSafari, content: {
+                    SafariView(url: recipeViewModel.recipeLink!)
+                        .ignoresSafeArea(edges: [.bottom])
+                })
             }
-            .background(Color.orange) // Listの背景色を設定
-            .listStyle(PlainListStyle()) // Listのスタイルを設定
-            
-            .navigationBarTitle("検索結果", displayMode: .inline)
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(Color.black, for: .navigationBar)
-            
-            .sheet(isPresented: $isShowSafari, content: {
-                SafariView(url: recipeViewModel.recipeLink!)
-                    .ignoresSafeArea(edges: [.bottom])
-            })
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.orange)
+            .edgesIgnoringSafeArea(.all)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.orange)
-        .edgesIgnoringSafeArea(.all)
     }
 }
